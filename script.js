dc96c09bed551d3858ce660c616bee5fd2c0de75
EEE95 <144793581+EEE95@users.noreply.github.com>
2024-08-27 10:13:37 +0200

    forslag

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ infoIcons.forEach(infoIcon => {
 });
 
 
-
+/*
 const avatars = document.querySelectorAll('.avatar');
 
 // Tilføj click event listeners til alle avatars
@@ -95,3 +95,50 @@ Hentning af data: På den næste side (f.eks. nextpage.html) henter vi den gemte
 </body>
 </html>
 */
+
+/*/ Definér et array med de 12 spørgsmål
+const spørgsmål = [
+    "Spørgsmål 1",
+    "Spørgsmål 2",
+    "Spørgsmål 3",
+    "Spørgsmål 4",
+    "Spørgsmål 5",
+    "Spørgsmål 6",
+    "Spørgsmål 7",
+    "Spørgsmål 8",
+    "Spørgsmål 9",
+    "Spørgsmål 10",
+    "Spørgsmål 11",
+    "Spørgsmål 12"
+  ];
+  
+  // Definér en variabel til at holde styr på hvilke spørgsmål der allerede er blevet valgt
+  let valgteSpørgsmål = [];
+  
+  // Funktion til at vælge et tilfældigt spørgsmål
+  function vælgTilfældigtSpørgsmål() {
+    // Fjern alle spørgsmål der allerede er blevet valgt fra arrayet
+    const tilgængeligeSpørgsmål = spørgsmål.filter(spørgsmål => !valgteSpørgsmål.includes(spørgsmål));
+  
+    // Vælg et tilfældigt spørgsmål fra de tilgængelige spørgsmål
+    const tilfældigtSpørgsmål = tilgængeligeSpørgsmål[Math.floor(Math.random() * tilgængeligeSpørgsmål.length)];
+  
+    // Tilføj det valgte spørgsmål til listen over valgte spørgsmål
+    valgteSpørgsmål.push(tilfældigtSpørgsmål);
+  
+    // Returnér det valgte spørgsmål
+    return tilfældigtSpørgsmål;
+  }
+  
+  // Funktion til at resætte spillet
+  function resætSpillet() {
+    // Nulstil listen over valgte spørgsmål
+    valgteSpørgsmål = [];
+  }
+  
+  // Eksempel på hvordan du kunne bruge funktionerne
+  for (let i = 0; i < 7; i++) {
+    console.log(vælgTilfældigtSpørgsmål());
+  }
+  
+  resætSpillet();
